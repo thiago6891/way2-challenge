10b215f59d17fb92de7ccc8aa2555b7bc32b7d12
Thiago Cordeiro <thiago6891@gmail.com>
2018-04-17 10:16:29 -0300

    client connecting to server.

---
+++ App/Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace App
+{
+    class Client
+    {
+        private string address;
+        private int port;
+
+        private ManualResetEvent connectDone = new ManualResetEvent(false);
+
+        public Client(string address, int port)
+        {
+            this.address = address;
+            this.port = port;
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                var ipAddress = IPAddress.Parse(address);
+                var remoteEP = new IPEndPoint(ipAddress, port);
+
+                var client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                client.BeginConnect(remoteEP, new AsyncCallback(ConnectCallback), client);
+                connectDone.WaitOne();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                var client = (Socket)ar.AsyncState;
+                client.EndConnect(ar);
+
+                Console.WriteLine("Socket connected to {0}", client.RemoteEndPoint.ToString());
+
+                connectDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace App
+﻿namespace App
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var client = new Client("40.121.85.141", 20010);
+            client.Connect();
         }
     }
 }
